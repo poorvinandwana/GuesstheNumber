7764ff839625f5ccb6cbaf084114ec3d03e76e5f
Poorvi Nandwana <146192656+poorvinandwana@users.noreply.github.com>
2025-09-09 20:00:18 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+import streamlit as st
+
+
+st.title("Guess the Number Game")
+
+if "number" not in st.session_state:
+    st.session_state.number = random.randint(1,100)
+    st.session_state.guesses = 0
+    st.session_state.solved = False
+
+if not st.session_state.solved:
+        st.text("I have selected a secret number.")
+        user = st.number_input("Enter your guess between 1 and 100", min_value=1, max_value=100,step=1)
+
+
+        if st.button("Submit"):
+            st.session_state.guesses += 1
+            if user < st.session_state.number:
+                st.warning("Higher number please!")
+            elif user > st.session_state.number:
+                st.warning("Lower number please!")
+            else:
+                st.session_state.solved = True
+                st.success(f"Congratulations! You guessed my secret number {st.session_state.number} in {st.session_state.guesses} guesses.")
+                st.balloons()
+
+else:
+    st.button("Play Again", on_click=lambda: [st.session_state.pop(k) for k in list(st.session_state.keys())])
+
